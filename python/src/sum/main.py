bcaf225a56687eb497d5ff1de845e53a12dd51f6
Pedro Ciliberto <pciliberto@fi.uba.ar>
2026-09-21 01:58:41 -0300

    Add client_id in sum to group fruit amounts by client

--- python/src/sum/main.py
+++ python/src/sum/main.py
@@ -24,32 +24,36 @@ class SumFilter:
                 MOM_HOST, AGGREGATION_PREFIX, [f"{AGGREGATION_PREFIX}_{i}"]
             )
             self.data_output_exchanges.append(data_output_exchange)
-        self.amount_by_fruit = {}
-
-    def _process_data(self, fruit, amount):
+        self.amount_by_client_and_fruit = {}
+        
+    def _process_data(self, client_id, fruit, amount):
         logging.info(f"Process data")
-        self.amount_by_fruit[fruit] = self.amount_by_fruit.get(
+        if client_id not in self.amount_by_client_and_fruit:
+            self.amount_by_client_and_fruit[client_id] = {}
+        client_dict = self.amount_by_client_and_fruit[client_id]
+        client_dict[fruit] = client_dict.get(
             fruit, fruit_item.FruitItem(fruit, 0)
         ) + fruit_item.FruitItem(fruit, int(amount))
 
-    def _process_eof(self):
+    def _process_eof(self, client_id):
         logging.info(f"Broadcasting data messages")
-        for final_fruit_item in self.amount_by_fruit.values():
+        client_dict = self.amount_by_client_and_fruit[client_id]
+        for final_fruit_item in client_dict.values():
             for data_output_exchange in self.data_output_exchanges:
                 data_output_exchange.send(
                     message_protocol.internal.serialize(
-                        [final_fruit_item.fruit, final_fruit_item.amount]
+                        [client_id, final_fruit_item.fruit, final_fruit_item.amount]
                     )
                 )
 
         logging.info(f"Broadcasting EOF message")
         for data_output_exchange in self.data_output_exchanges:
-            data_output_exchange.send(message_protocol.internal.serialize([]))
+            data_output_exchange.send(message_protocol.internal.serialize([client_id]))
 
 
     def process_data_messsage(self, message, ack, nack):
         fields = message_protocol.internal.deserialize(message)
-        if len(fields) == 2:
+        if len(fields) == 3:
             self._process_data(*fields)
         else:
             self._process_eof(*fields)
