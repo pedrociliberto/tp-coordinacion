e0c222d6277f67347e672a0472ff23f7b20d67f7
Pedro Ciliberto <pciliberto@fi.uba.ar>
2026-09-21 02:05:10 -0300

    Add client_id in aggregation to create partial tops by client

--- python/src/aggregation/main.py
+++ python/src/aggregation/main.py
@@ -23,21 +23,25 @@ class AggregationFilter:
         self.output_queue = middleware.MessageMiddlewareQueueRabbitMQ(
             MOM_HOST, OUTPUT_QUEUE
         )
-        self.fruit_top = []
+        self.fruit_tops_by_client = {}
 
-    def _process_data(self, fruit, amount):
+    def _process_data(self, client_id, fruit, amount):
         logging.info("Processing data message")
-        for i in range(len(self.fruit_top)):
-            if self.fruit_top[i].fruit == fruit:
-                self.fruit_top[i] = self.fruit_top[i] + fruit_item.FruitItem(
+        if client_id not in self.fruit_tops_by_client:
+            self.fruit_tops_by_client[client_id] = []
+        client_top = self.fruit_tops_by_client[client_id]
+        for i in range(len(client_top)):
+            if client_top[i].fruit == fruit:
+                client_top[i] = client_top[i] + fruit_item.FruitItem(
                     fruit, amount
                 )
                 return
-        bisect.insort(self.fruit_top, fruit_item.FruitItem(fruit, amount))
+        bisect.insort(client_top, fruit_item.FruitItem(fruit, amount))
 
-    def _process_eof(self):
+    def _process_eof(self, client_id):
         logging.info("Received EOF")
-        fruit_chunk = list(self.fruit_top[-TOP_SIZE:])
+        client_top = self.fruit_tops_by_client.get(client_id, [])
+        fruit_chunk = list(client_top[-TOP_SIZE:])
         fruit_chunk.reverse()
         fruit_top = list(
             map(
@@ -45,13 +49,14 @@ class AggregationFilter:
                 fruit_chunk,
             )
         )
-        self.output_queue.send(message_protocol.internal.serialize(fruit_top))
-        del self.fruit_top
+        self.output_queue.send(message_protocol.internal.serialize([client_id, fruit_top]))
+        if client_id in self.fruit_tops_by_client:
+            del self.fruit_tops_by_client[client_id]
 
     def process_messsage(self, message, ack, nack):
         logging.info("Process message")
         fields = message_protocol.internal.deserialize(message)
-        if len(fields) == 2:
+        if len(fields) == 3:
             self._process_data(*fields)
         else:
             self._process_eof()
